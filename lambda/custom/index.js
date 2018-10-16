63172a1a1d9f99732bb97c045d149119435824cb
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2018-10-16 20:03:09 +0200

    add: alexa skill skeleton
    Signed-off-by: kevinhassan <kevin.hassan@etu.umontpellier.fr>

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,8 +1,86 @@
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!')
-    };
-    return response;
+'use strict';
+
+import { handler } from 'alexa-sdk';
+
+const handlers = {
+    //Use LaunchRequest, instead of NewSession if you want to use the one-shot model
+    // Alexa, ask [my-skill-invocation-name] to (do something)...
+    'LaunchRequest': function () {
+        this.attributes.speechOutput = this.t('WELCOME_MESSAGE', this.t('SKILL_NAME'));
+        // If the user either does not reply to the welcome message or says something that is not
+        // understood, they will be prompted again with this text.
+        this.attributes.repromptSpeech = this.t('WELCOME_REPROMPT');
+
+        this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
+        this.emit(':responseReady');
+    },
+    'RecipeIntent': function () {
+        const itemSlot = this.event.request.intent.slots.Item;
+        let itemName;
+        if (itemSlot && itemSlot.value) {
+            itemName = itemSlot.value.toLowerCase();
+        }
+
+        const cardTitle = this.t('DISPLAY_CARD_TITLE', this.t('SKILL_NAME'), itemName);
+        const myRecipes = this.t('RECIPES');
+        const recipe = myRecipes[itemName];
+
+        if (recipe) {
+            this.attributes.speechOutput = recipe;
+            this.attributes.repromptSpeech = this.t('RECIPE_REPEAT_MESSAGE');
+
+            this.response.speak(recipe).listen(this.attributes.repromptSpeech);
+            this.response.cardRenderer(cardTitle, recipe);
+            this.emit(':responseReady');
+        } else {
+            let speechOutput = this.t('RECIPE_NOT_FOUND_MESSAGE');
+            const repromptSpeech = this.t('RECIPE_NOT_FOUND_REPROMPT');
+            if (itemName) {
+                speechOutput += this.t('RECIPE_NOT_FOUND_WITH_ITEM_NAME', itemName);
+            } else {
+                speechOutput += this.t('RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME');
+            }
+            speechOutput += repromptSpeech;
+
+            this.attributes.speechOutput = speechOutput;
+            this.attributes.repromptSpeech = repromptSpeech;
+
+            this.response.speak(speechOutput).listen(repromptSpeech);
+            this.emit(':responseReady');
+        }
+    },
+    'AMAZON.HelpIntent': function () {
+        this.attributes.speechOutput = this.t('HELP_MESSAGE');
+        this.attributes.repromptSpeech = this.t('HELP_REPROMPT');
+
+        this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
+        this.emit(':responseReady');
+    },
+    'AMAZON.RepeatIntent': function () {
+        this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
+        this.emit(':responseReady');
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit('SessionEndedRequest');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit('SessionEndedRequest');
+    },
+    'SessionEndedRequest': function () {
+        console.log(`Session ended: ${this.event.request.reason}`);
+    },
+    'Unhandled': function () {
+        this.attributes.speechOutput = this.t('HELP_MESSAGE');
+        this.attributes.repromptSpeech = this.t('HELP_REPROMPT');
+        this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
+        this.emit(':responseReady');
+    },
 };
+
+export function handler (event, context, callback) {
+    const alexa = handler(event, context, callback);
+    alexa.APP_ID = APP_ID;
+    // To enable string internationalization (i18n) features, set a resources object.
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+}
