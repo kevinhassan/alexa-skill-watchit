ef72ba9877701563e3fea8215ec81e3319cf8cef
AgbodjogbeYves-alain <agboniel@yahoo.fr>
2018-10-18 00:19:12 +0200

    [ADD] Feature synopsis

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,87 +1,82 @@
-/* eslint-disable  func-names */
-/* eslint quote-props: ["error", "consistent"]*/
-/**
- * This sample demonstrates a simple skill built with the Amazon Alexa Skills
- * nodejs skill development kit.
- * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
- * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
- * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
- **/
-
 'use strict';
 const Alexa = require('alexa-sdk');
-
-//=========================================================================================================================================
-//TODO: The items below this comment need your attention.
-//=========================================================================================================================================
-
-//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
-//Make sure to enclose your value in quotes, like this: const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
-const APP_ID = undefined;
-
-const SKILL_NAME = 'Space Facts';
-const GET_FACT_MESSAGE = "Here's your fact: ";
-const HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... What can I help you with?';
-const HELP_REPROMPT = 'What can I help you with?';
-const STOP_MESSAGE = 'Goodbye!';
-
-//=========================================================================================================================================
-//TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/data
-//=========================================================================================================================================
-const data = [
-    'A year on Mercury is just 88 days long.',
-    'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-    'Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.',
-    'On Mars, the Sun appears about half the size as it does on Earth.',
-    'Earth is the only planet not named after a god.',
-    'Jupiter has the shortest day of all the planets.',
-    'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-    'The Sun contains 99.86% of the mass in the Solar System.',
-    'The Sun is an almost perfect sphere.',
-    'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-    'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-    'The temperature inside the Sun can reach 15 million degrees Celsius.',
-    'The Moon is moving approximately 3.8 cm away from our planet every year.',
-];
-
-//=========================================================================================================================================
-//Editing anything below this line might break your skill.
-//=========================================================================================================================================
+const https = require('https');
 
 const handlers = {
-    'LaunchRequest': function () {
-        this.emit('GetNewFactIntent');
+    'LaunchRequest': function() {
+        this.emit(':ask', 'Bonjour et bienvenue dans la skill WatchIt que puis-je faire pour vous ?');
     },
-    'GetNewFactIntent': function () {
-        const factArr = data;
-        const factIndex = Math.floor(Math.random() * factArr.length);
-        const randomFact = factArr[factIndex];
-        const speechOutput = GET_FACT_MESSAGE + randomFact;
+    'GetSynopsisFilmIntent': function() {
+        // Make a request for a user with a given ID
+        const movieName = cleanUpSpecialChars(this.event.request.intent.slots.movie.value);
+        const negativeResponseSynopsis = [
+            'Désolé je ne parviens pas à retrouver le résumé du film' + movieName,
+            'Je n\'ai pas pu trouver le résumé du film que vous avez demandé',
+            'Oups je n\ai pas trouver le résume du film' + movieName
+        ];
 
-        this.response.cardRenderer(SKILL_NAME, randomFact);
-        this.response.speak(speechOutput);
-        this.emit(':responseReady');
+        https.get('https://api.betaseries.com/movies/search?key=fc9ace0877d3&title=' + movieName, (resp) => {
+            let data = '';
+
+            // A chunk of data has been recieved.
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            // The whole response has been received. Print out the result.
+            resp.on('end', () => {
+
+                const moviesData = JSON.parse(data)
+                if (moviesData.movies.length != 0) {
+                    const speechOutput = moviesData.movies[0].synopsis
+                    this.response.speak("Le résumé de " + movieName + " est :" + speechOutput) //.emit(':responseReady');
+                    this.emit(':responseReady');
+
+                    //this.emit(':ask', "Que puis-je faire d'autres pour vous ?");
+                } else {
+                    const responseIndex = Math.floor(Math.random() * Math.floor(negativeResponseSynopsis.length));
+                    this.response.speak(negativeResponseSynopsis[responseIndex])
+                    this.emit(':responseReady');
+
+                    //this.emit(':ask', "Que puis-je faire d'autres pour vous ?");
+                }
+            });
+
+        })
+
+
+        /*.catch(function(error) {
+                        this.response.speak("Impossible de trouver le résumé de ce film");
+                        this.emit(':ask', "Que puis-je faire d'autres pour vous ?");
+                    })*/
     },
-    'AMAZON.HelpIntent': function () {
+    'AMAZON.HelpIntent': function() {
         const speechOutput = HELP_MESSAGE;
         const reprompt = HELP_REPROMPT;
 
         this.response.speak(speechOutput).listen(reprompt);
         this.emit(':responseReady');
     },
-    'AMAZON.CancelIntent': function () {
+    'AMAZON.CancelIntent': function() {
         this.response.speak(STOP_MESSAGE);
         this.emit(':responseReady');
     },
-    'AMAZON.StopIntent': function () {
+    'AMAZON.StopIntent': function() {
         this.response.speak(STOP_MESSAGE);
         this.emit(':responseReady');
     },
 };
 
-exports.handler = function (event, context, callback) {
+function cleanUpSpecialChars(str) {
+    return str
+        .replace(/[ÀÁÂÃÄÅ]/g, "A")
+        .replace(/[àáâãäå]/g, "a")
+        .replace(/[ÈÉÊË]/g, "E")
+        .replace(/[èéêë]/g, "e")
+}
+
+exports.handler = function(event, context, callback) {
     const alexa = Alexa.handler(event, context, callback);
-    alexa.APP_ID = APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
