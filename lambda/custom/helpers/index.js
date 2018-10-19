8b8597c3a436fbbf48d8af08babf9c1cef23d226
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2018-10-19 22:56:57 +0200

    add: choiceIntent
    Signed-off-by: kevinhassan <kevin.hassan@etu.umontpellier.fr>

---
+++ lambda/custom/helpers/index.js
@@ -0,0 +1,5 @@
+const linkHelper = require('./linkHelper')
+
+module.exports = {
+  linkHelper
+}
--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,76 +1,64 @@
-'use strict';
-const Alexa = require('alexa-sdk');
-const https = require('https');
-var tools = require('./util');
+'use strict'
+const Alexa = require('alexa-sdk')
+const axios = require('axios')
+const Helpers = require('./helpers')
 
 const handlers = {
-    'LaunchRequest': function() {
-        this.emit(':ask', 'Bonjour et bienvenue dans la skill WatchIt que puis-je faire pour vous ?');
-    },
-    'GetSynopsisFilmIntent': function() {
-        // Make a request for a user with a given ID
-        const movieName = tools.clean(this.event.request.intent.slots.movie.value);
-        const negativeResponseSynopsis = [
-            'Désolé je ne parviens pas à retrouver le résumé du film' + movieName,
-            'Je n\'ai pas pu trouver le résumé du film que vous avez demandé',
-            'Oups je n\ai pas trouver le résume du film' + movieName
-        ];
-
-        https.get('https://api.betaseries.com/movies/search?key=fc9ace0877d3&title=' + movieName, (resp) => {
-            let data = '';
-
-            // A chunk of data has been recieved.
-            resp.on('data', (chunk) => {
-                data += chunk;
-            });
-
-            // The whole response has been received. Print out the result.
-            resp.on('end', () => {
-
-                const moviesData = JSON.parse(data)
-                if (moviesData.movies.length != 0) {
-                    const speechOutput = moviesData.movies[0].synopsis
-                    this.response.speak("Le résumé de " + movieName + " est :" + speechOutput) //.emit(':responseReady');
-                    this.emit(':responseReady');
-
-                    //this.emit(':ask', "Que puis-je faire d'autres pour vous ?");
-                } else {
-                    const responseIndex = Math.floor(Math.random() * Math.floor(negativeResponseSynopsis.length));
-                    this.response.speak(negativeResponseSynopsis[responseIndex])
-                    this.emit(':responseReady');
-
-                    //this.emit(':ask', "Que puis-je faire d'autres pour vous ?");
-                }
-            });
-
+  'LaunchRequest': function () {
+    this.emit(':ask', 'Bonjour et bienvenue dans la skill WatchIt. Voulez-vous des informations sur une série ou un film ?', 'Voulez-vous des informations sur une série ou un film ?')
+  },
+  'ChoiceIntent': function () {
+    if (this.event.request.intent.slots.choice) {
+      const choice = this.event.request.intent.slots.choice
+      this.attributes.choice = (choice === 'film' || choice === 'serie') ? choice : ''
+      return this.emit(':ask', 'Quel information voulez-vous obtenir ?').listen("Veuillez indiquer l'information que vous voulez obtenir ?")
+    }
+  },
+  'GetSynopsisIntent': function () {
+    const title = this.event.request.intent.slots.title
+    if (this.attributes.choice) {
+      axios.get(Helpers.linkHelper(this.attributes.choice, title))
+        .then(function (response) {
+          if (response.data) {
+            // TODO: renvoyer les 3 premiers résultats
+            const synopsis = (this.attributes.choice === 'film') ? response.data.movies[0] : response.data.shows[0]
+            this.attributes.title = title
+            // TODO: ne pas lire tout le résumé d'un coup
+            this.response.speak(synopsis)
+            return this.emit(':responseReady')
+          } else {
+            return this.emit(':ask', "Je n'ai pas trouvé de résumé concernant : " + title, 'Sur quel ' + this.attributes.choice + ' voulez-vous obtenir le résumé ?').listen()
+          }
         })
+        .catch(function (err) {
+          console.error(err)
+          // TODO: utiliser un error handler de alexa ?
+          return this.emit(':ask', 'Une erreur est survenue. Veuillez réessayer.').listen()
+        })
+    } else {
+      return this.emit(':ask', 'Est-ce une série ou un film ?', "Veuillez indiquer s'il s'agit d'une série ou d'un film")
+    }
+  },
+  'AMAZON.HelpIntent': function () {
+    const speechOutput = HELP_MESSAGE
+    const reprompt = HELP_REPROMPT
 
+    this.response.speak(speechOutput).listen(reprompt)
+    this.emit(':responseReady')
+  },
+  'AMAZON.CancelIntent': function () {
+    this.response.speak(STOP_MESSAGE)
+    this.emit(':responseReady')
+  },
+  'AMAZON.StopIntent': function () {
+    this.response.speak(STOP_MESSAGE)
+    this.emit(':responseReady')
+  }
+}
 
-        /*.catch(function(error) {
-                        this.response.speak("Impossible de trouver le résumé de ce film");
-                        this.emit(':ask', "Que puis-je faire d'autres pour vous ?");
-                    })*/
-    },
-    'AMAZON.HelpIntent': function() {
-        const speechOutput = HELP_MESSAGE;
-        const reprompt = HELP_REPROMPT;
-
-        this.response.speak(speechOutput).listen(reprompt);
-        this.emit(':responseReady');
-    },
-    'AMAZON.CancelIntent': function() {
-        this.response.speak(STOP_MESSAGE);
-        this.emit(':responseReady');
-    },
-    'AMAZON.StopIntent': function() {
-        this.response.speak(STOP_MESSAGE);
-        this.emit(':responseReady');
-    },
-};
-
-
-exports.handler = function(event, context, callback) {
-    const alexa = Alexa.handler(event, context, callback);
-    alexa.registerHandlers(handlers);
-    alexa.execute();
-};
+exports.handler = function (event, context, callback) {
+  const alexa = Alexa.handler(event, context, callback)
+  alexa.appId = process.env.APP_ID
+  alexa.registerHandlers(handlers)
+  alexa.execute()
+}
